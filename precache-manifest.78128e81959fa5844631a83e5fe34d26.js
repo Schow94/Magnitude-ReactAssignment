e75aaf5ddeb0f987361b9ef9ce9920b5f459e7ad
Schow94 <Schow94@gmail.com>
2019-10-07 08:53:55 -0700

    Updates

---
+++ precache-manifest.78128e81959fa5844631a83e5fe34d26.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "404aabd4ce57d5abfb8affe133a1d6fd",
+    "url": "/Magnitude-ReactAssignment/index.html"
+  },
+  {
+    "revision": "f4d0cd3034fd854aee7c",
+    "url": "/Magnitude-ReactAssignment/static/js/2.b258ea9b.chunk.js"
+  },
+  {
+    "revision": "e87715dc5c438156c7b1",
+    "url": "/Magnitude-ReactAssignment/static/js/main.2257893f.chunk.js"
+  },
+  {
+    "revision": "e58c8b079ac4c8684892",
+    "url": "/Magnitude-ReactAssignment/static/js/runtime-main.cd517fe7.js"
+  }
+]);
